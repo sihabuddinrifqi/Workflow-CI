0d41538d9f7b87b31946f1eff3dbe5c10b1c95a1
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-19 21:25:55 +0700

    Refactor MLflow logging in modelling.py
    Refactored MLflow logging by removing unnecessary start_run context and directly logging parameters and metrics.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -8,33 +7,33 @@ from sklearn.preprocessing import StandardScaler
 
 
 def main():
+    # Experiment boleh diset
     mlflow.set_experiment("CI_RFM_Clustering")
 
+    # Load data
     df = pd.read_csv("dataset_preprocessing.csv")
 
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
-    # 🔑 Attach ke run MLflow Project
-    run_id = os.environ.get("MLFLOW_RUN_ID")
+    # Train model
+    kmeans = KMeans(
+        n_clusters=4,
+        random_state=42
+    )
+    kmeans.fit(X_scaled)
 
-    with mlflow.start_run(run_id=run_id):
-        kmeans = KMeans(
-            n_clusters=4,
-            random_state=42
-        )
-        kmeans.fit(X_scaled)
+    # Logging TANPA start_run
+    mlflow.log_param("n_clusters", 4)
+    mlflow.log_param("random_state", 42)
+    mlflow.log_metric("inertia", kmeans.inertia_)
 
-        mlflow.log_param("n_clusters", 4)
-        mlflow.log_param("random_state", 42)
-        mlflow.log_metric("inertia", kmeans.inertia_)
+    mlflow.sklearn.log_model(
+        kmeans,
+        artifact_path="kmeans_model"
+    )
 
-        mlflow.sklearn.log_model(
-            kmeans,
-            artifact_path="kmeans_model"
-        )
-
-        print("CI training selesai & artefak tersimpan.")
+    print("CI training selesai & artefak tersimpan.")
 
 
 if __name__ == "__main__":
