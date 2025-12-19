ea22e0a0b62de1f915b9cc2d69b906a4b27ccd0b
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-19 21:04:59 +0700

    Simplify KMeans model training and logging
    Refactor KMeans training and logging process in main function.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,33 +7,31 @@ from sklearn.preprocessing import StandardScaler
 
 
 def main():
-    mlflow.set_experiment("CI_RFM_Clustering")
-
+    # Load dataset
     df = pd.read_csv("dataset_preprocessing.csv")
 
+    # Scaling
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
-    # 🔑 WAJIB nested=True karena dijalankan via MLflow Project
-    with mlflow.start_run(nested=True):
+    # Train model
+    kmeans = KMeans(
+        n_clusters=4,
+        random_state=42
+    )
+    kmeans.fit(X_scaled)
 
-        kmeans = KMeans(
-            n_clusters=4,
-            random_state=42
-        )
-        kmeans.fit(X_scaled)
+    # ===== MANUAL LOGGING (SKILLED) =====
+    mlflow.log_param("n_clusters", 4)
+    mlflow.log_param("random_state", 42)
+    mlflow.log_metric("inertia", kmeans.inertia_)
 
-        # Manual logging (SKILLED)
-        mlflow.log_param("n_clusters", 4)
-        mlflow.log_param("random_state", 42)
-        mlflow.log_metric("inertia", kmeans.inertia_)
+    mlflow.sklearn.log_model(
+        kmeans,
+        artifact_path="kmeans_model"
+    )
 
-        mlflow.sklearn.log_model(
-            kmeans,
-            artifact_path="kmeans_model"
-        )
-
-        print("CI training selesai & artefak tersimpan.")
+    print("CI training selesai & artefak tersimpan.")
 
 
 if __name__ == "__main__":
