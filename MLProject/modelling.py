7f72389f1d6d4905f51b4b3522c263c15e658632
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-19 20:50:37 +0700

    Refactor KMeans training and logging process
    Refactor KMeans model training and logging in main function.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,37 +4,38 @@ import mlflow.sklearn
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_score
 
 
 def main():
-    # Set tracking ke local folder
-    mlflow.set_tracking_uri("file:./mlruns")
+    # Set experiment
     mlflow.set_experiment("CI_RFM_Clustering")
 
-    # PAKSA start run baru (CI-safe)
-    with mlflow.start_run(run_name="ci_training_run"):
-        # Load dataset
-        df = pd.read_csv("dataset_preprocessing.csv")
+    # Load dataset
+    df = pd.read_csv("dataset_preprocessing.csv")
 
-        # Scaling
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(df)
+    # Scaling
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(df)
 
-        # Train model
-        model = KMeans(n_clusters=4, random_state=42)
-        model.fit(X_scaled)
+    # Model
+    kmeans = KMeans(
+        n_clusters=4,
+        random_state=42
+    )
+    kmeans.fit(X_scaled)
 
-        # Evaluation
-        labels = model.predict(X_scaled)
-        sil_score = silhouette_score(X_scaled, labels)
+    # ================= LOGGING MANUAL (SKILLED) =================
+    mlflow.log_param("n_clusters", 4)
+    mlflow.log_param("random_state", 42)
 
-        # Manual logging (Skilled)
-        mlflow.log_param("n_clusters", 4)
-        mlflow.log_metric("silhouette_score", sil_score)
-        mlflow.sklearn.log_model(model, "model")
+    mlflow.log_metric("inertia", kmeans.inertia_)
 
-        print("Training CI selesai & artefak tersimpan")
+    mlflow.sklearn.log_model(
+        kmeans,
+        artifact_path="kmeans_model"
+    )
+
+    print("CI training selesai & artefak tersimpan.")
 
 
 if __name__ == "__main__":
