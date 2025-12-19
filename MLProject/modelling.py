35b718bd2c59a7dc5534ee957ba46cf1b3ec1dd0
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-19 21:17:53 +0700

    Add MLflow experiment setup and logging

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,27 +1,25 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 
 def main():
-    # Load dataset
+    mlflow.set_experiment("CI_RFM_Clustering")
+
     df = pd.read_csv("dataset_preprocessing.csv")
 
-    # Scaling
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
-    # Train model
     kmeans = KMeans(
         n_clusters=4,
         random_state=42
     )
     kmeans.fit(X_scaled)
 
-    # ===== MANUAL LOGGING (SKILLED) =====
+    # Manual logging (SKILLED)
     mlflow.log_param("n_clusters", 4)
     mlflow.log_param("random_state", 42)
     mlflow.log_metric("inertia", kmeans.inertia_)
