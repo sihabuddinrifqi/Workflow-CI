332ee95c450a5b505b01fa00287eaf8a8e865399
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-19 21:22:55 +0700

    Integrate KMeans with MLflow for logging

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,6 +1,8 @@
+import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
@@ -13,23 +15,26 @@ def main():
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
-    kmeans = KMeans(
-        n_clusters=4,
-        random_state=42
-    )
-    kmeans.fit(X_scaled)
+    # 🔑 Attach ke run MLflow Project
+    run_id = os.environ.get("MLFLOW_RUN_ID")
 
-    # Manual logging (SKILLED)
-    mlflow.log_param("n_clusters", 4)
-    mlflow.log_param("random_state", 42)
-    mlflow.log_metric("inertia", kmeans.inertia_)
+    with mlflow.start_run(run_id=run_id):
+        kmeans = KMeans(
+            n_clusters=4,
+            random_state=42
+        )
+        kmeans.fit(X_scaled)
 
-    mlflow.sklearn.log_model(
-        kmeans,
-        artifact_path="kmeans_model"
-    )
+        mlflow.log_param("n_clusters", 4)
+        mlflow.log_param("random_state", 42)
+        mlflow.log_metric("inertia", kmeans.inertia_)
 
-    print("CI training selesai & artefak tersimpan.")
+        mlflow.sklearn.log_model(
+            kmeans,
+            artifact_path="kmeans_model"
+        )
+
+        print("CI training selesai & artefak tersimpan.")
 
 
 if __name__ == "__main__":
