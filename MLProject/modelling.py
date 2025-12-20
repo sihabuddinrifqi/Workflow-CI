e9972e3bd975fb578c3baecf56ffdcbad8739940
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-20 11:14:09 +0700

    Refactor main function in modelling.py for clarity

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,27 +8,20 @@ from sklearn.preprocessing import StandardScaler
 def main():
     mlflow.set_experiment("CI_RFM_Clustering")
 
-    with mlflow.start_run():
+    df = pd.read_csv("dataset_preprocessing.csv")
 
-        df = pd.read_csv("dataset_preprocessing.csv")
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(df)
 
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(df)
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    kmeans.fit(X_scaled)
 
-        kmeans = KMeans(
-            n_clusters=4,
-            random_state=42
-        )
-        kmeans.fit(X_scaled)
+    mlflow.log_param("n_clusters", 4)
+    mlflow.log_param("random_state", 42)
+    mlflow.log_metric("inertia", kmeans.inertia_)
 
-        mlflow.log_param("n_clusters", 4)
-        mlflow.log_param("random_state", 42)
-        mlflow.log_metric("inertia", kmeans.inertia_)
+    mlflow.sklearn.log_model(kmeans, artifact_path="kmeans_model")
 
-        mlflow.sklearn.log_model(
-            kmeans,
-            artifact_path="kmeans_model"
-        )
 
         print("CI training selesai & artefak tersimpan.")
 
