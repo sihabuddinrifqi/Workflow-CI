46e0c0ac63778c5af00e922a59925d81a1c13b28
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-20 11:59:05 +0700

    Refactor KMeans training and logging process
    Refactor KMeans model training and MLflow logging.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,28 +5,32 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 def main():
-    # JANGAN gunakan mlflow.set_experiment di sini karena akan menyebabkan 
-    # ketidaksinkronan ID saat dijalankan via 'mlflow run'.
-    
+
+    # Load data
     df = pd.read_csv("dataset_preprocessing.csv")
+
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
-    kmeans = KMeans(n_clusters=4, random_state=42)
+    # Train model
+    kmeans = KMeans(
+        n_clusters=4,
+        random_state=42
+    )
     kmeans.fit(X_scaled)
 
-    # Menggunakan blok start_run() tanpa parameter agar otomatis menyambung
-    # ke Run ID yang sudah dibuat oleh perintah 'mlflow run'
-    with mlflow.start_run():
-        mlflow.log_param("n_clusters", 4)
-        mlflow.log_param("random_state", 42)
-        mlflow.log_metric("inertia", kmeans.inertia_)
+    # Langsung log tanpa context manager
+    # MLflow Project otomatis mendeteksi run yang sedang aktif
+    mlflow.log_param("n_clusters", 4)
+    mlflow.log_param("random_state", 42)
+    mlflow.log_metric("inertia", kmeans.inertia_)
 
-        mlflow.sklearn.log_model(
-            kmeans,
-            artifact_path="kmeans_model"
-        )
-        print("Training selesai & artefak tersimpan.")
+    mlflow.sklearn.log_model(
+        kmeans,
+        artifact_path="kmeans_model"
+    )
+
+    print("CI training selesai & artefak tersimpan.")
 
 if __name__ == "__main__":
     main()
