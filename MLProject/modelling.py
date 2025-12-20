c844494f67d66a3d1b97b0dd6cfa301074586dc0
sihabuddinrifqi <137701488+sihabuddinrifqi@users.noreply.github.com>
2025-12-20 11:55:26 +0700

    Refactor KMeans model logging with mlflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,25 +4,29 @@ import mlflow.sklearn
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-
 def main():
-    mlflow.set_experiment("CI_RFM_Clustering")
-
+    # JANGAN gunakan mlflow.set_experiment di sini karena akan menyebabkan 
+    # ketidaksinkronan ID saat dijalankan via 'mlflow run'.
+    
     df = pd.read_csv("dataset_preprocessing.csv")
-
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
-    model = KMeans(n_clusters=4, random_state=42)
-    model.fit(X_scaled)
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    kmeans.fit(X_scaled)
 
-    mlflow.log_param("n_clusters", 4)
-    mlflow.log_metric("inertia", model.inertia_)
+    # Menggunakan blok start_run() tanpa parameter agar otomatis menyambung
+    # ke Run ID yang sudah dibuat oleh perintah 'mlflow run'
+    with mlflow.start_run():
+        mlflow.log_param("n_clusters", 4)
+        mlflow.log_param("random_state", 42)
+        mlflow.log_metric("inertia", kmeans.inertia_)
 
-    mlflow.sklearn.log_model(model, artifact_path="kmeans_model")
-
-    print("Training selesai, model tersimpan sebagai artefak MLflow")
-
+        mlflow.sklearn.log_model(
+            kmeans,
+            artifact_path="kmeans_model"
+        )
+        print("Training selesai & artefak tersimpan.")
 
 if __name__ == "__main__":
     main()
